4f69356b77317e8b4d75bae4ddb4bfb73ea71117
Evgeny <arswarog@yandex.ru>
2021-07-06 12:33:15 +0300

    Дополнил задания

--- arrays-hw/helpers.js
+++ arrays-hw/helpers.js
@@ -17,3 +17,22 @@ function markByStudent(student) {
 function getFIOShort(student) {
     return `${student.lastname} ${student.firstname.substr(0, 1)}. ${student.patronymic.substr(0, 1)}.`;
 }
+
+/**
+ * Вычисление среднего арифметического
+ * @param {Array<number>} data массив чисел
+ * @return {number} среднее арифметическое
+ */
+function average(data) {
+    const sum = data.reduce((acc, item) => acc + item);
+    return sum / data.length;
+}
+
+/**
+ * Округляет до одного знака после запятой
+ * @param  {number} value значение для округления
+ * @return {number} результат округления
+ */
+function prettyRound(value) {
+    return Math.round(value * 10) / 10;
+}
--- arrays-hw/script.js
+++ arrays-hw/script.js
@@ -1,12 +1,17 @@
 console.log(`Всего студентов в базе: ${allStudents.length}`);
 console.log(`Всего оценок в базе: ${allMarks.length}`);
 
-console.log(
-    'Количество оценок у каждого студента:',
-    allStudents
+console.log('0. Количество оценок у каждого студента:');
+console.table(marksCountPerStudent(allStudents, allMarks));
+
+function marksCountPerStudent(students, marks) {
+    return students
         .map((student) => {
-            const markCount = allMarks.filter(markByStudent(student)).length;
+            const count = marks.filter(markByStudent(student)).length;
             const fio = getFIOShort(student);
-            return `${fio}: ${markCount}`;
-        })
-);
+            return {
+                fio,
+                count,
+            };
+        });
+}
