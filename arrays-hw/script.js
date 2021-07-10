1aa1198c7230f72ae6f19d3443b8b9adc5fb040b
PKO <itsmart@itsmart.io>
2021-07-10 19:15:55 +0300

    Work on lesson

--- arrays-hw/script.js
+++ arrays-hw/script.js
@@ -15,3 +15,38 @@ function marksCountPerStudent(students, marks) {
             };
         });
 }
+
+function mostPopularGenderStudents() {
+    const fem = allStudents.filter((item) => item.gender === "f").length
+    const man = allStudents.filter((item) => item.gender === "m").length
+    if (fem > man) {
+        return 'f';
+    }
+    else if (man > fem) {
+        return 'm';
+    }
+    else {
+        return '=';
+    }
+
+}
+
+console.log(mostPopularGenderStudents());
+
+function countOfStudent(group) {
+    return allStudents.filter((item) => item.group === group).length
+}
+
+console.log(countOfStudent('АТП-071'));
+console.log(countOfStudent('АТП-081'));
+
+function numberOfFive(group) {
+    const studIDs = allStudents
+        .filter((item) => item.group === group)
+        .map((item) => item.id);
+    return allMarks
+        .filter((item) => item.mark === 5)
+        .filter((item) => studIDs.includes(item.studentId))
+        .length
+}
+console.log(numberOfFive("ФК-071"))
