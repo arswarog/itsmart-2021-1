811cb440c980ca21d9d0893057a8a09c2a9cda5e
PKO <itsmart@itsmart.io>
2021-07-17 20:48:29 +0300

    on-class

---
+++ rzhd/types.js
@@ -0,0 +1,13 @@
+/**
+ * Информация о станции
+ * @typedef {Object} Station
+ * @property {number} id Первичный ключ
+ * @property {string} name Название
+ */
+
+/**
+ * Информация о маршруте
+ * @typedef {Object} Route
+ * @property {string} name Номер маршрута (Первичный ключ)
+ * @property {number[]} stations
+ */
