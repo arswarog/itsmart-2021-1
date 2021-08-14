4701c482393df487469951e34252ed6ffcc9ed4f
Arswarog <arswarog@yandex.ru>
2021-08-14 21:18:31 +0300

    CW 14.08.21

--- rzhd/rzhd.js
+++ rzhd/rzhd.js
@@ -13,34 +13,42 @@ function stationFactory(name) {
 }
 
 /**
- * 
- * @param {string} name 
- * @param {number[]} stations 
+ * Создает объект маршрута
+ * @param {string} id Номер маршрута
+ * @param {number[]} stations
  * @returns Route
  */
-function routeFactory(name, stations) {
+function routeFactory(id, stations) {
+    stations.forEach(id => {
+        if (!findStationById(id))
+            throw new Error(`Станции ${id} не существует`);
+    });
+
+    const firstStation = findStationById(stations[0]);
+    const lastStation = findStationById(stations[stations.length - 1]);
+
     return {
-        name,
+        id,
+        name: `${firstStation.name} - ${lastStation.name}`,
         stations
     }
 }
 
 /**
  * Выводим на экран весь маршрут
- * @param {Route} route 
- * 
+ * @param {Route} route
+ *
  */
 function printRoute(route) {
-    console.log('Route: ', route.name)
+    console.log(`Route #${route.id} ${route.name}`)
     console.table(route.stations.map(
-        (id) => stations.find(item => item.id === id)
+        (id) => allStations.find(item => item.id === id)
     ))
 }
 
-
 /**
  * Поиск маршрутов
- * 
+ *
  * @param {number} start Станция отправления
  * @param {number} end Станция прибытия
  * @returns {Route[]}
@@ -57,30 +65,50 @@ function findRoutes(start, end) {
 
 /**
  * Поиск рейсов
- * @param {number} start 
- * @param {number} end 
- * @param {string} date 
+ * @param {number} start
+ * @param {number} end
+ * @param {string} date
  */
 function findReises(start, end, date) {
-    const routes = findRoutes(start, end);
-    return allReises
-        .filter(filterByRoutes(routes))
-        .filter(filterByDate(date));
+    try {
+        const routes = findRoutes(start, end);
+        return allReises
+            .filter(filterByRoutes(routes))
+            .filter(filterByDate(date));
+    } catch (e) {
+        throw new Error('Ошибка при поиске рейса\n' + e.message);
+    }
 }
 
 /**
  * Поиск рейсов (для отображения пользователю)
  * @param {string} start Название станции отправления
- * @param {string} end Название станции прибытия 
+ * @param {string} end Название станции прибытия
  * @param {string} date Дата отправления
  */
 function findReisesForPassengers(start, end, date) {
-    const startStaion = findStation(start);
-    const endStaion = findStation(end);
-    const reises = findReises(startStaion.id, endStaion.id, date);
-    return reises
-    // .map((reis) => {
+    try {
+        const startStation = findStation(start);
+        if (!startStation)
+            throw new Error('Неверная станция отправления')
 
+        const endStation = findStation(end);
+        if (!endStation)
+            throw new Error('Неверная станция прибытия')
+
+        const reises = findReises(startStation.id, endStation.id, date);
+
+        console.log(`Найденные рейсы по направлению ${startStation.name} - ${endStation.name} на дату ${date}`);
+        if (reises.length)
+            console.log(reises);
+        else
+            console.log('Ничего не найдено');
+        return reises
+    } catch (error) {
+        alert('Извините, что-то пошло не так\n' + error.message);
+        return [];
+    }
+    // .map((reis) => {
     //     return {
     //         reisId,
     //         date,
@@ -91,9 +119,10 @@ function findReisesForPassengers(start, end, date) {
     //     }
     // });
 }
+
 /**
  * Возвращает объект из массива stotions по имени станции
- * @param {string} stationName 
+ * @param {string} stationName
  * @returns {Station}
  */
 function findStation(stationName) {
@@ -102,8 +131,19 @@ function findStation(stationName) {
 }
 
 /**
+ * Возвращает станцию по id
+ * @param {number} stationId
+ * @returns {Station} station
+ */
+function findStationById(stationId) {
+    return allStations
+        .find((item) => stationId === item.id);
+
+}
+
+/**
  * Возвращает функцию, которая фильтрует массив рейсов по маршрутам
- * @param {Route[]} routes 
+ * @param {Route[]} routes
  * @returns {function}
  */
 function filterByRoutes(routes) {
@@ -111,14 +151,14 @@ function filterByRoutes(routes) {
      * Функция проверяет, идет ли рейс по маршруту из routes
      */
     return (reise) => routes
-        .find((route) => route.name === reise.routeId);
+        .find((route) => route.id === reise.routeId);
 }
 
 /**
  * Возвращает функцию, которая проверяет идет ли рейс в указанную дату
- * @param {string} date 
+ * @param {string} date
  * @returns {function}
  */
 function filterByDate(date) {
-   return (reise) => reise.date === date; 
+    return (reise) => reise.date === date;
 }
